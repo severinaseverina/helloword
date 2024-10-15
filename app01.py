a89a2d628f16113a57e1931160e0e0d86dc7ec90
severinaseverina <sandraonacred@gmail.com>
2024-10-15 11:57:11 -0300

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,40 @@
+# Cria uma variável
+
+"""Esse comentário pode
+te muitas
+e muitas
+linhas"""
+
+
+
+"""Esse comentário pode
+te muitas
+e muitas
+linhas"""
+# Cria uma variável usando "snake_case"
+cor_da_casa = 'azul'
+
+# Criar uma variável usando "camelCase"
+corDaCasa = "amarela"
+
+# Exibe as coisas
+print('\n\n\n')
+print(cor_da_casa)
+print(corDaCasa)
+
+
+# Primeiro nome
+first_name = 'Gabriel'
+# Ultimo nome
+last_name = "Silva"
+
+# Imprime meu nome
+print(first_name)
+print(last_name)
+
+# Imprime os dois nomes
+print(first_name, last_name)
+
+
+
+
